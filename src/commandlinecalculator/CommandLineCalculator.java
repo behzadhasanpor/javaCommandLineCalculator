531c62990eef8dc006f813e28c378e82a432e118
Behzad <behzadhasanpor@yahoo.com>
2018-07-03 03:16:02 +0430

    adding files to repository

---
+++ src/commandlinecalculator/CommandLineCalculator.java
@@ -0,0 +1,36 @@
+/*
+--------------------------------------------------------------------------------------
+
+
+*main calculator moderator class*
+
+
+---------------------------------------------------------------------------------------
+*/
+package commandlinecalculator;
+
+import java.util.Scanner;
+
+public class CommandLineCalculator {
+    public static void main(String[] args) {
+        while(true){
+            System.out.print(" KBase -> ");
+            Scanner input=new Scanner(System.in);
+            try{
+            String inp=input.nextLine().trim();
+                if("exit".equals(inp) || "end".equals(inp)){System.out.println("Bye!");break;}
+                System.out.println(new calculator(inp).solvePattern());
+            }
+            catch(Exception exp){
+                System.out.println("Please check the command out and try again!");
+            }
+        }
+    }
+    
+}
+/*
+--------------------------------------------------------------------------------------
+MIT licence@KBase -> https://github.com/behzadhasanpor
+---------------------------------------------------------------------------------------
+*/
+
